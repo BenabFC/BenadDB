3367e902702d3825ada56c4e3a6e72c1c0560700
ArchiouBlueCo <achiroublueco@gmail.com>
2024-12-13 14:52:49 +0000

    Add files via upload

---
+++ client/src/components/Dashboard.jsx
@@ -0,0 +1,133 @@
+import React, {useState, useEffect} from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
+import Datazone from './Datazone'
+import Settings from './Settings'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAdd, faCog , faHome} from '@fortawesome/free-solid-svg-icons';
+
+import './css/Dashboard.css'
+import './css/responsive/Dashboard.css'
+import AddForm from './AddForm'
+
+const Dashboard = () => {
+
+  const location = useLocation();
+  const [loginId, setLoginId] = useState(); 
+  const [scoutName, setScoutName] = useState(); 
+  // const scoutName = location.state?._scoutName; // Access the state passed via navigate
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const [selectedComponent, setSelectedComponent] = useState('A');
+
+  useEffect(() => {
+    // const location = useLocation();
+    setLoginId(location.state?.message);
+    setScoutName(location.state?._scoutName); // Access the state passed via navigate
+
+    const verifyToken = async () => {
+        const token = localStorage.getItem('jwtToken');
+        if (!token) {
+            navigate('/'); // Redirect to homepage if no token
+            return;
+        }
+        try {
+            navigate("/dashboard");
+        } catch (error) {
+            localStorage.removeItem('jwtToken'); // Clear invalid token
+            navigate('/'); // Redirect to homepage
+        }
+    };
+
+      verifyToken();
+  }, [navigate]);
+
+  useEffect(() => {
+    let timer;
+    const resetTimer = () => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            localStorage.removeItem('jwtToken'); // Clear token
+            window.location.href = '/'; // Redirect to homepage
+        }, 5 * 60 * 1000); // 5 minutes inactivity
+    };
+
+    window.addEventListener('mousemove', resetTimer);
+    window.addEventListener('keypress', resetTimer);
+
+    resetTimer(); // Initialize timer
+
+    return () => {
+        clearTimeout(timer);
+        window.removeEventListener('mousemove', resetTimer);
+        window.removeEventListener('keypress', resetTimer);
+    };
+}, []);
+
+  // Function to handle switching of components
+  const handleComponentChange = (component) => {
+    setSelectedComponent(component);
+  };
+
+  // Function to render the selected form component
+  const renderComponent = () => {
+    switch (selectedComponent) {
+      case 'A':
+        return <Datazone key="A" scoutName={scoutName}/>; // Provide unique key to force re-mount
+      case 'B':
+        return <AddForm key="B" scoutName={scoutName}/>;
+      case 'C':
+        return <Settings key="C" scoutId={loginId}/>;
+      default:
+        return <Datazone key="A" scoutName={scoutName}/>;
+    }
+  }
+  const handleLogout = () => {
+    
+    localStorage.removeItem("jwtToken");  // Remove JWT token
+    navigate("/");  // Redirect back to login page
+  };
+
+
+
+  return (
+    <div className="holder">
+
+      {/* <Header/> */}
+
+      <div className="dash holder">
+        <div className='sidebar'>
+          <div className="logo">
+              <img src="/chelsea.png" alt="" />
+              <h4>Achirou BlueCo</h4>
+          </div>
+          <ul>
+              <li onClick={() => handleComponentChange(`A`)}>
+                  <div className="dashIcon"><FontAwesomeIcon icon={faHome} /></div>
+                  <span>Dashboard</span>
+              </li>
+              <li onClick={() => handleComponentChange(`B`)}>
+                  <div className="dashIcon"><FontAwesomeIcon icon={faAdd} /></div>
+                  <span>Add Player</span>
+              </li>
+              <li onClick={() => handleComponentChange(`C`)}>
+                  <div className="dashIcon"><FontAwesomeIcon icon={faCog}/></div>
+                  <span>Change Password</span>
+              </li>
+          </ul>
+          <div className="signout">
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        </div>
+        
+        {/* Conditionally rendered component */}
+        <div className="component-container">
+          {renderComponent()}
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ client/src/components/EvaluationView.jsx
@@ -0,0 +1,196 @@
+import React, {useState, useEffect} from 'react'
+import './css/EvaluationView.css'
+import viteLogo from '/vite.svg';
+import { apiService } from "../services/apiService.js";
+import ExportStyledPDF from './ExportStyledPDF.jsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAdd } from '@fortawesome/free-solid-svg-icons';
+
+const EvaluationView = ({player}) => {
+
+    const [ratings, setRatings] = useState([{}]);
+    const [isRate, setIsRate] = useState(false);
+    const [err, setErr] = useState();
+    const [link, setLink] = useState({url: '', title: ''});
+    const [linkList, setLinkList] = useState([]);
+
+    const fetchPlayerLinks = async () =>{
+        try{
+            const search = `/players/getlinks/${player._id}/`;
+            var linkSearch = await apiService.get(search);
+            setLinkList(linkSearch.Link); 
+        }catch (e){
+            console.log("Error loading links: " + e);
+        }
+    } 
+
+    useEffect(() => {
+        //Fetch all players from the database
+          const fetchEvaluation = async () => {
+            try {
+              const query = `/evaluation/${player._id}/`;
+              const data = await apiService.get(query);
+              setRatings(data);
+              setIsRate(true);
+            } catch (e) {
+              setErr("Failed to load evaluation: " + e);
+              console.log(err);
+            }
+          };
+          fetchEvaluation();
+          fetchPlayerLinks();
+        }, []);
+    
+        const formatDate = (isoDate) => {
+            const date = new Date(isoDate);
+            return date.toLocaleDateString();  // This will format the date according to the user's locale
+        };
+
+        const renderEvaluation = () => {
+            return Object.entries(ratings).map(([key, value]) => {
+                const header = key.replace(/_/g, " ");
+                if(!(key.toUpperCase() === "PLAYER_ID" || key.toUpperCase() === "_ID" || key.toUpperCase() === "NOTE")){
+                    return (
+                        <div key={key} className="noteView">
+                            <span>{header}</span>
+                            <div className="noteValue">
+                                {value}
+                            </div>
+                        </div>
+                    ); 
+                }
+                
+            });
+        }
+
+         // Handle changes for input fields
+         const handleInputChange = (e) => {
+            const { name, value } = e.target;
+            setLink((prevData) => ({
+            ...prevData,
+            [name]: value,
+            }));
+        };
+
+        const emptyLinkFields = () =>{
+            setLink({url: "", title: ""});
+        }
+
+        const addLink = async (e) =>{
+            e.preventDefault();
+            
+            try {
+                const jsonData = {
+                    url: link.url,
+                    title: link.title 
+                }
+                const response = await apiService.put(`/players/addlink/${player._id}`, jsonData, {
+                  headers: {
+                    'Content-Type': "application/json",
+                  },
+                });
+                fetchPlayerLinks();
+                emptyLinkFields();
+              } catch (error) {
+                console.error('Error adding link:', error);
+              }
+            }
+
+  return (
+    <div className="view_wrapper">
+        <div className="view_personal">
+            <div className="personal_top">
+                <div className="img">
+                    <img src={player.Image} alt="" />
+                </div>
+                <h2>{player.First_name + " " + player.Last_name}</h2>
+            </div>
+            <div className="personal_btm">
+                <div className="personal_detail">
+                    <span>Date of Birth: </span>
+                    <p>{formatDate(player.Date_of_Birth)}</p>
+                </div>
+                <div className="personal_detail">
+                    <span>Prefered Foot: </span>
+                    <p>{player.Preferred_Foot}</p>
+                </div>
+                <div className="personal_detail">
+                    <span>Height: </span>
+                    <p>{player.Height== "" ? "N/A" : `${player.Height} cm`}</p>
+                </div>
+                <div className="personal_detail">
+                    <span>Position: </span>
+                    <p>{player.Position}</p>
+                </div>
+                <div className="personal_detail">
+                    <span>Nationality: </span>
+                    <p>{player.Nationality}</p>
+                </div>
+                <div className="personal_detail">
+                    <span>Club: </span>
+                    <p>{player.Club}</p>
+                </div>
+                <div className="personal_detail">
+                    <span>Scouted by: </span>
+                    <p>{player.Scouted_By}</p>
+                </div>
+                <div className="personal_detail">
+                    <span>Region Scouted: </span>
+                    <p>{player.Region_scouted_in}</p>
+                </div>
+                <div className="personal_detail">
+                    <span>Agent: </span>
+                    <p>{player.Agent}</p>
+                </div>
+                <div className="personal_detail">
+                    <span>Contract Length: </span>
+                    <p>{player.Contract == null ? "N/A" : `Until ${player.Contract}`}</p>
+                </div>
+                <div className="personal_detail">
+                    <span>Date Added: </span>
+                    <p>{formatDate(player.Date_Added)}</p>
+                </div>
+            <ExportStyledPDF player={player} evaluation={ratings}/>    
+            </div>
+        </div>
+
+        <div className="view_evaluation">
+            <div className="player-evaluation">
+            <h3>Evaluation Metrics</h3>
+            <div  className="evaluation-metrics">
+                {isRate && renderEvaluation()} 
+            </div>
+            </div>
+
+            <div className="player-evaluation">
+                {/* <div className="border" style={{borderTop: "solid 1px var(--text)"}}></div> */}
+                <h4 className="links-header">Player Videos</h4>
+                {/* <div className="border" style={{borderTop: "solid 1px var(--text)"}}></div> */}
+                <form onSubmit={addLink} className="video-link">
+                    <div className="link-input">
+                        <input type="text" placeholder="Paste link here" value={link.url} name='url' onChange={handleInputChange} required className='xca'/>
+                        <input type="text" placeholder="Title" value={link.title} name='title' onChange={handleInputChange} required/>
+                    </div>
+                    <button type="submit" className='addLink'>
+                        <FontAwesomeIcon icon={faAdd}/>
+                    </button>
+                </form>
+                <ul className="linkList">
+                    {
+                        Object.entries(linkList).map(([index,i])=>{
+                            const formattedUrl = i.url.startsWith("http") ? i.url : `https://${i.url}`;
+                            return (
+                                <li key={(linkList.length + 5)} className="link">
+                                    <a href={formattedUrl} className="link" target="_blank" rel="noopener noreferrer">{i.title}</a>
+                                </li>
+                            )
+                        })
+                    }                    
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default EvaluationView
